35badb38325b5d36713b48bddfd4a1b38f21d7ef
Nils ANDRÉ-CHANG <contact@nils-andre-chang.com>
2017-09-06 20:04:01 +0100

    Continued GamePanel and Game

--- src/fr/nils/object/Game.java
+++ src/fr/nils/object/Game.java
@@ -8,22 +8,20 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
+import java.util.Observable;
 
-public class Game implements ActionListener {
+public class Game extends Observable implements ActionListener {
 	private String wordRevealed;
 	private String wordShowed;
 	private String wordNoAccent; //
 	private final String DICTIONNARYFILE = "dictionnaire.txt";
 	private final int WORDNUMBER = 336531;
-	private final char accentTable[][] = {{'é', 'à', 'è', 'ù', 'â', 'ê', 'î', 'ô','û', 'ç', 'ë', 'ï', 'ü'},{'e', 'a', 'e', 'u', 'a', 'e', 'i', 'o', 'u', 'c', 'e', 'i', 'u'}};
+	private final char accentTable[][] = { { 'é', 'à', 'è', 'ù', 'â', 'ê', 'î', 'ô', 'û', 'ç', 'ë', 'ï', 'ü' },
+			{ 'e', 'a', 'e', 'u', 'a', 'e', 'i', 'o', 'u', 'c', 'e', 'i', 'u' } };
 	private LineNumberReader lnr;
+	public Score score = new Score();
 
 	public Game() {
-		
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
 
 	}
 
@@ -31,21 +29,26 @@ public class Game implements ActionListener {
 		int randomNum = (int) (Math.random() * WORDNUMBER);
 		try {
 			lnr = new LineNumberReader(new BufferedReader(new FileReader(new File(DICTIONNARYFILE))));
-			for(int x=0;x<randomNum;x++) {
+			for (int x = 0; x < randomNum; x++) {
 				lnr.readLine();
 			}
 			wordRevealed = lnr.readLine();
 			lnr.close();
-			System.out.println("number :"+randomNum+", word :"+lnr); // To remove (debug)
+			System.out.println("number :" + randomNum + ", word :" + lnr); // To remove (debug)
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
-			
+
 		}
 		wordNoAccent = wordRevealed;
-		for(int y=0;y < accentTable[0].length;y++) {
+		for (int y = 0; y < accentTable[0].length; y++) {
 			wordNoAccent = wordNoAccent.replace(accentTable[0][y], accentTable[1][y]);
 		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		
 	}
 
 }
--- src/fr/nils/vue/GamePanel.java
+++ src/fr/nils/vue/GamePanel.java
@@ -1,7 +1,43 @@
 package fr.nils.vue;
 
+import java.awt.BorderLayout;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel {
+import fr.nils.object.Game;
 
+public class GamePanel extends JPanel implements Observer {
+	private JPanel keyboard = new JPanel();
+	private final String NUMWORDSTR = "Nombre de mot actuel : ";
+	private final String SCORESTR = "Votre score actuel est de : ";
+	private JLabel numWord;
+	private JLabel score;
+	private Game game = new Game();
+
+	public GamePanel() {
+		game.addObserver(this);
+
+		JPanel jPanel = new JPanel();
+		jPanel.setLayout(new BorderLayout());
+		
+		numWord = new JLabel(NUMWORDSTR + game.score.getNumWord());
+		score = new JLabel(SCORESTR + game.score.getScore());
+		JPanel infos = new JPanel();
+		infos.setLayout(new BorderLayout());
+		infos.add(numWord, BorderLayout.NORTH);
+		infos.add(score, BorderLayout.SOUTH);
+		jPanel.add(infos, BorderLayout.NORTH);
+		
+		
+
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		this.numWord.setText(NUMWORDSTR + game.score.getNumWord());
+		this.score.setText(SCORESTR + game.score.getScore());
+	}
 }
